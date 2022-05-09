42c5c62c8472cf6abd20a6698fad87b9da1011eb
Imtiaz Ahmed Chowdhury <imtiazemon625@gmail.com>
2022-05-09 13:36:52 +0600

    Single Products Hooks working Fine

--- src/Pages/Hooks/useSingleProduct.js
+++ src/Pages/Hooks/useSingleProduct.js
@@ -3,7 +3,13 @@ import axios from "axios";
 
 const useSingleProduct = (productId)=>{
     const [product,setProduct] = useState({});
-   
+    useEffect(()=>{
+        axios.get(`https://salty-fjord-90713.herokuapp.com/inventory/${productId}`)
+        .then(response =>{
+            const {data} = response;
+          setProduct(data)
+        }) 
+     },[])
      return [product,setProduct]
 }
 export default useSingleProduct;
