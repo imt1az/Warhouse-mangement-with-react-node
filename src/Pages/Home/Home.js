e8004905bfb13006fafeeb20af17e798c1a2d12e
Imtiaz Ahmed Chowdhury <imtiazemon625@gmail.com>
2022-05-09 11:57:02 +0600

    Banner Add to The HomePage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Products from './Products/Products';
 
 const Home = () => {
     return (
-        <div className='container mx-auto'>
+        <div className=''>
+            <Banner></Banner>
            <Products></Products>
         </div>
     );
