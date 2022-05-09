511f5a30eef93a869c4d466c1f88750b1003a1a8
Imtiaz Ahmed Chowdhury <imtiazemon625@gmail.com>
2022-05-09 13:35:27 +0600

    All Product Hooks Working Fine

--- src/Pages/Hooks/useProducts.js
+++ src/Pages/Hooks/useProducts.js
@@ -5,7 +5,13 @@ import { useEffect, useState } from "react";
 const useProducts = ()=>{
     const [products,setProducts] = useState([]);
 
-   
+    useEffect(()=>{
+        axios.get('https://salty-fjord-90713.herokuapp.com/products')
+        .then(response =>{
+            const {data} = response;
+            setProducts(data);
+        })
+    },[])
     return [products,setProducts] 
 }
 export default useProducts;
